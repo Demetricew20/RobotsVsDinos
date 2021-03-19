81f24161a01d77a266a3a7c244887d1703c339b0
DemetriceW20 <demetrice_williams@hotmail.com>
2021-03-19 10:03:34 -0800

    All battlefield, weapon, and attack options now accept lowercase and uppercase inputs

--- battlefield.py
+++ battlefield.py
@@ -6,10 +6,17 @@ class Battlefield:
 
         print(self.battlefields)
         self.user_input = input("Select battlefield from list: ")
+        option_list = self.battlefields.copy()
+        lower = []
+        upper = []
+        for options in option_list:
+            lower.append(options.lower())
+            upper.append(options.upper())
 
-        while self.user_input not in self.battlefields:
+        while self.user_input not in self.battlefields and self.user_input not in lower and self.user_input not in upper:
             self.user_input = input("Try Again: ")
-
+        else:
+            self.user_input = self.user_input.capitalize()
 
     def __repr__(self):
         return f'\nBattlefield: {self.user_input}\n'
--- weapons.py
+++ weapons.py
@@ -1,5 +1,6 @@
 from copy import deepcopy
 
+
 class Weapons:
     def __init__(self):
         self.weapon_list = ["Sword", "Cannon", "Fist", "Rock"]
@@ -7,20 +8,30 @@ class Weapons:
         print(f'Available Weapons: {self.weapon_list}\nAvailable Attacks: {self.attack_list}\n')
 
     def choose_weapon(self, robo_obj):
-        lst = self.weapon_list.copy()
+        option_list = self.weapon_list.copy()
         weapon_choice = input(f'Select weapon from list for {robo_obj.name} : ')
-        while weapon_choice not in lst:
+        lower = []
+        upper = []
+        for item in option_list:
+            lower.append(item.lower())
+            lower.append(item.upper())
+        while weapon_choice not in option_list and weapon_choice not in lower and weapon_choice not in upper:
             weapon_choice = input("Try again: ")
         else:
-            robo_obj.weapon = weapon_choice
+            robo_obj.weapon = weapon_choice.capitalize()
 
     def choose_attack(self, dino_obj):
-        lst = deepcopy(self.attack_list)
+        option_list = deepcopy(self.attack_list)
         attack_choice = input(f'Select weapon from list for {dino_obj.type} : ')
-        while attack_choice not in lst:
+        lower = []
+        upper = []
+        for item in option_list:
+            lower.append(item.lower())
+            lower.append(item.upper())
+        while attack_choice not in option_list and attack_choice not in lower and attack_choice not in upper:
             attack_choice = input("Try again: ")
         else:
-            dino_obj.attack = attack_choice
+            dino_obj.attack = attack_choice.capitalize()
 
 
 
